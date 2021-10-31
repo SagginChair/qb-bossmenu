1c226ce3b62cad2fe1bcffa007b8ad973f36b377
SagginChair <Vlezxsaggin1@gmail.com>
2021-10-31 14:19:55 -0500

    blackmarket boss menu

--- config.lua
+++ config.lua
@@ -7,6 +7,6 @@ Config.Jobs = {
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
     ['mechanic'] = vector3(-1427.88, -457.98, 35.91),
-    ['blackmarket'] = vector3(0.1, 9.98, 35.91),
+    ['blackmarket'] = vector3(-593.23, 217.41, 74.15),
     ['burgershot'] = vector3(-1192.04, -902.476, 13.998),
 }
