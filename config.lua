42611d1339f499a6abdd32ab0d4e68550ae4b042
SagginChair <Vlezxsaggin1@gmail.com>
2021-12-13 12:22:44 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,14 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(460.68, -985.54, 30.73),
-    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['police'] = vector3(460.44, -985.46, 30.73),
+    ['ambulance'] = vector3(270.5, -1363.0, 24.5),
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-1427.88, -457.98, 35.91),
+    ['blackmarket'] = vector3(-593.23, 217.41, 74.15),
+    ['burgershot'] = vector3(-1192.04, -902.476, 13.998),
+    ['whitewidow'] = vector3(182.26, -251.58, 54.07),
+    ['stripclub'] = vector3(94.93, -1292.74, 29.26),
 }
